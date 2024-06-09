3ec72fa778bcb224c9abc20c701fb72665939cdf
baecareles <justaamirashraf@gmail.com>
2024-06-09 15:54:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import StopWatch from './Components/StopWatch/StopWatch';
+
+function App() {
+  
+	return (
+    
+		<div className="App">
+      <h1>STOP WATCH</h1>
+			<StopWatch/>
+		</div>
+	);
+}
+
+export default App;
